0ea4efcbb0624505be40c948423c81638c685f70
Ahmed Yasser <ahmdyassr@gmail.com>
2022-08-28 02:23:46 +0200

    Add env variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
-const connectionURL = 'mongodb+srv://ahmdyassr:0109105349@todoa-app.lrnbe.mongodb.net/?retryWrites=true&w=majority';
+const connectionURL = process.env.MONGO_URI;
 mongoose.connect(connectionURL);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ require('./db/mongoose');
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT
 
 
 // Middleware
